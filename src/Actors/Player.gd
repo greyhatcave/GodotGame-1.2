355adb4c7684895036cb72a38f67c2dcd1364517
Ario124 <patrick__solis@hotmail.com>
2020-02-23 02:11:30 +0100

    Updated 'fireball' & player with get_global_mouse_position

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,20 +1,14 @@
 extends Actor
-export var stomp_impulse: = 1000.0
 
-
-func _on_EnemyDetector_area_entered(area):
-	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
-	
-func _on_EnemyDetector_body_entered(body):
-	queue_free()
 
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-
-
+	
+	
+	
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
@@ -41,9 +35,4 @@ func calculate_move_velocity(
 		out.y = speed.y * direction.y 
 	if is_jump_interrupted:
 		out.y = 0.0
-	return out
-
-func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
-	var out: = linear_velocity
-	out.y = -impulse
 	return out
