eebe821c52b6537df860b40894d832fd2f6c8d42
Ario124 <47329632+Ario124@users.noreply.github.com>
2020-08-03 19:31:52 +0200

    Updated Level_Template, changed some settings on Camera2D.

--- src/Actors/Camera2D.gd
+++ src/Actors/Camera2D.gd
@@ -6,13 +6,26 @@ const SHIFT_EASE = Tween.EASE_OUT
 const SHIFT_DURATION = 1.0
 
 var facing = 0
+var parent = get_parent()
 
 onready var prev_camera_pos = get_camera_position()
 onready var tween = $ShiftTween
+onready var player = get_node("/root/Level01/Player")
 
 func _process(_delta):
 	_check_facing()
 	prev_camera_pos = get_camera_position()
+	
+	#Test for camera position
+	if player.position.y < 1009:
+		drag_margin_v_enabled = true
+		position.y = -508
+	
+	if player.position.y > 1009:
+		drag_margin_v_enabled = true
+		position.y = -85
+		drag_margin_v_enabled = false
+
 
 func _check_facing():
 	var new_facing = sign(get_camera_position().x - prev_camera_pos.x)
