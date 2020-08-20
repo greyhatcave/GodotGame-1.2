41c2de65e5bdc7175d19ae2528d28b3987131354
Ario124 <47329632+Ario124@users.noreply.github.com>
2020-08-20 23:32:10 +0200

    Timers for bullets

--- src/Objects/Gun.gd
+++ src/Objects/Gun.gd
@@ -1,9 +1,25 @@
 extends KinematicBody2D
 
 onready var parent = get_parent()
+
+var timer = null
+var bullet_delay = 0.4
+var can_shoot = true
 var fireball = preload("res://src/Objects/Fireball.tscn")
 
 export var fireball_speed = 1500
+
+
+
+func _ready():
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(bullet_delay)
+	timer.connect("timeout", self, "on_timeout_complete")
+	add_child(timer)
+
+func on_timeout_complete():
+	can_shoot = true
 
 
 func _process(_delta):
@@ -13,7 +29,7 @@ func _process(_delta):
 		
 # Call shoot in player.tscn::1 with 'child'
 func shoot(_delta):
-	if Input.is_action_just_pressed("fire") && parent.player_alive:
+	if Input.is_action_just_pressed("fire") && can_shoot:#parent.player_alive:
 		var fireball_instance = fireball.instance()
 		fireball_instance.position = $FirePointer.get_global_position()
 		fireball_instance.rotation = get_angle_to(get_global_mouse_position())
@@ -24,3 +40,5 @@ func shoot(_delta):
 		if !parent.velocity:
 			fireball_instance.apply_impulse(Vector2(), Vector2(fireball_speed, 0).rotated(rotation + rand_range(-0.05, 0.05)))
 		get_tree().get_root().add_child(fireball_instance)
+		can_shoot = false
+		timer.start()
--- src/Objects/Launcher.gd
+++ src/Objects/Launcher.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var coin_value = 1
 
-export var speed = 550
+export var speed = 350
 export var steer_force = 400.0
 var parent = get_parent()
 var damage = 10
@@ -11,12 +11,35 @@ var velocity = Vector2.ZERO
 var acceleration = Vector2.ZERO
 var target = null
 
+var timer = null
+var bullet_delay = 4
+var can_shoot = true
+
 onready var degota = get_tree().get_root().get_node("Level01").get_node("Degota")
 onready var player = get_tree().get_root().get_node("Level01").get_node("Player")
 
+
+
+
+func _ready():
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(bullet_delay)
+	timer.connect("timeout", self, "on_timeout_complete")
+	add_child(timer)
+
+func on_timeout_complete():
+	can_shoot = true
+
 func _process(_delta):
-	if degota.can_fire == false:
+	if degota.can_fire == false && can_shoot:
 		set_physics_process(true)
+		can_shoot = false
+		timer.start()
+	if can_shoot:
+		queue_free()
+	if player.player_alive == false:
+		queue_free()
 
 func _physics_process(delta):
 	acceleration += seek()
@@ -44,3 +67,7 @@ func _on_Launcher_body_shape_entered(_body_id, body, _body_shape, _area_shape):
 	if body.is_in_group("Player"):
 		body.PlayerOnHit(damage)
 	queue_free()
+
+
+func _on_Timer_timeout():
+	print("TIME OUT")
