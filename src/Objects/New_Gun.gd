74d3fd0a055646ab14633131f9c014077119bfde
Ario124 <47329632+Ario124@users.noreply.github.com>
2020-09-11 22:35:04 +0200

    Added a new weapon, Use '1' and '2' to change weapon

---
+++ src/Objects/New_Gun.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+onready var parent = get_parent()
+
+var timer = null
+var fire_rate = 0.7
+var bullet_delay = 0.1
+var can_fire = true
+var fireball = preload("res://src/Objects/New_Gun_Projectile.tscn")
+
+
+func _ready():
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(bullet_delay)
+	timer.connect("timeout", self, "on_timeout_complete")
+	add_child(timer)
+
+func on_timeout_complete():
+	can_fire = true
+
+
+func _process(_delta):
+	
+	if parent.current_weapon == 2:
+		shoot(_delta)
+
+
+func shoot(_delta):
+	if Input.is_action_pressed("fire") and can_fire == true:
+		var projectiles_angles = [0]
+		var projectiles = 4
+		for i in range(1, projectiles * 2):
+			var num = float(i) / 8
+			projectiles_angles.append(num)
+			projectiles_angles.append(num * -1)
+	
+		for i in range(projectiles):
+			var bullet_instance = fireball.instance()
+			var angle = get_angle_to(get_global_mouse_position())
+			angle += projectiles_angles[i]
+			bullet_instance.position = $FirePointer.global_position
+			bullet_instance.rotation = angle
+			get_tree().get_root().add_child(bullet_instance)
+	
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
